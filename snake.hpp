f5229ed6a2ddef77215e1aaa2263c6649785ecc8
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2021-12-19 18:19:23 +0300

    snake3

--- snake.hpp
+++ snake.hpp
@@ -9,6 +9,11 @@ class Snake
 public:
     Snake(const stf::Vec2d startPos = { 10, 10 });
 
+    inline void feed() { m_body.push_back(m_body.back()); }
+
+    inline const stf::Vec2d& head() const { return m_body.front(); }
+    inline const std::vector<stf::Vec2d>& body() const { return m_body; }
+
 private:
     std::vector<stf::Vec2d> m_body;
     stf::Vec2d m_vel = {0,0};

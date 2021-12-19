5c557cc7349e4e38a69213a9cb5dbb5c32cb8dcf
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2021-12-19 20:36:40 +0300

    snake11

--- main.cpp
+++ main.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
 #include "window.hpp"
 #include "snake.hpp"
+#include "random.hpp"
 
 using namespace std;
 using namespace stf;
 
 class Game : public Window
 {
+    stf::Vec2d eat {21, 4};
     Snake snake;
     float t = 0.f;
 public:
     bool onUpdate(const float dt) override
     {
-        if(t > 1.f)
+        if(t > .025f)
         {
+            if(snake.head().x > eat.x) snake.A();
+            if(snake.head().x < eat.x) snake.D();
+            if(snake.head().y > eat.y) snake.W();
+            if(snake.head().y < eat.y) snake.S();
             snake.update();
+            if(snake.head().diff(eat) < 1.f) {
+                snake.feed(); eat = Vec2d(Random(time(0)).getNum(2, renderer.Size.x-1),
+                                          Random(time(0)).getNum(2, renderer.Size.y-1));
+            }
+            if(snake.isAteHerself()) return false;
+            snake.wrapping(1,1, renderer.Size.x, renderer.Size.y);
             t = 0.f;
         }
         snake.show(renderer);
+        renderer.drawPixel(eat, '~');
         t += dt;
         return true;
     }

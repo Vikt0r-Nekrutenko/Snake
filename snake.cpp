f92be51baabf970b0a041540ab97ad849d75b796
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2021-12-19 18:16:05 +0300

    snake2

--- snake.cpp
+++ snake.cpp
@@ -1,6 +1,13 @@
 #include "snake.hpp"
 
-Snake::Snake()
+Snake::Snake(const stf::Vec2d startPos)
 {
+    for(int i = 0; i < 5; ++i)
+        m_body.push_back({startPos.x + i, startPos.y});
+}
 
+void Snake::moveBody()
+{
+    for(auto it = m_body.end()-1; it > m_body.begin(); --it)
+        *it = *(it - 1);
 }
--- snake.hpp
+++ snake.hpp
@@ -7,11 +7,13 @@
 class Snake
 {
 public:
-    Snake();
+    Snake(const stf::Vec2d startPos = { 10, 10 });
 
 private:
     std::vector<stf::Vec2d> m_body;
     stf::Vec2d m_vel = {0,0};
+
+    void moveBody();
 };
 
 #endif // SNAKE_HPP

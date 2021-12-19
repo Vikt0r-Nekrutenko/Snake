0b0784f799f95fcbb8e11dcead99584441ccf4f7
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2021-12-19 20:47:52 +0300

    model3

--- model.cpp
+++ model.cpp
@@ -10,6 +10,8 @@ Signal Model::onUpdate(const float dt)
 {
     if(m_time > 0.3f)
     {
+        m_snake.update();
+        m_snake.wrapping(1,1,m_mapSize.x, m_mapSize.y);
         m_time = 0.f;
     }
     m_time += dt;

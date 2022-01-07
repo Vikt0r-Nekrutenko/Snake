1c36988e6e2da438c71d3e8faeda361ad372fb09
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-01-07 16:26:00 +0200

    snake refactoring 3

--- snake.cpp
+++ snake.cpp
@@ -3,7 +3,7 @@
 
 Snake::Snake(const stf::Vec2d startPos)
 {
-    for(int i = 0; i < 5; ++i) {
+    for(unsigned int i = 0; i < snake_settings::DEF_LENGHT; ++i) {
         m_body.push_back({startPos.x + i, startPos.y});
     }
 }
--- snake.hpp
+++ snake.hpp
@@ -3,10 +3,15 @@
 
 #include "segmentedentity.hpp"
 
+namespace snake_settings {
+const stf::Vec2d DEF_START_POS { 10, 10 };
+constexpr unsigned int DEF_LENGHT = 5;
+}
+
 class Snake : public SegmentedEntity
 {
 public:
-    Snake(const stf::Vec2d startPos = { 10, 10 });
+    Snake(const stf::Vec2d startPos = snake_settings::DEF_START_POS);
 
     void update() override;
     void feed() override;

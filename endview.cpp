990c3bc2b7f8f2bddfa0b5a95d9fc0bb0e9e4b0d
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-01-28 21:25:48 +0200

    012

--- endview.cpp
+++ endview.cpp
@@ -6,7 +6,7 @@ EndView::EndView(stf::smv::BaseModel *model)
 
 void EndView::show(stf::Renderer &renderer)
 {
-    m_end.show(renderer, renderer.Size / 2 - m_end.Size() / 2 - stf::Vec2d(0, 11));
+    m_end.show(renderer, renderer.Size / 2 - m_end.Size() / 2 - stf::Vec2d(0, 9));
     renderer.draw(renderer.Size / 2 - stf::Vec2d(4, 0), "SCORE: %d", model<GameModel>()->hunterModels().at(0)->score());
     renderer.draw(renderer.Size / 2 - stf::Vec2d(4, 2), "LEVEL: %d", model<GameModel>()->hunterModels().at(0)->lvl());
 }
